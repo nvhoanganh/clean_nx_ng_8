9be68c5c3754c4003af4d9bedaea6299608223f3
anthony <nvhoanganh1909@gmail.com>
2019-08-22 23:40:03 +1000

    clear state

--- apps/avatar/src/app/app.module.ts
+++ apps/avatar/src/app/app.module.ts
@@ -1,30 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { CoreModule } from '@avatar/core'
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(
-      {},
-      {
-        metaReducers: !environment.production ? [] : [],
-        runtimeChecks: {
-          strictActionImmutability: true,
-          strictStateImmutability: true
-        }
-      }
-    ),
-    EffectsModule.forRoot([]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
     CoreModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
