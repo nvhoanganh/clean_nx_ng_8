f9d543bfa249f54f01cae42a31b3ae6655debf04
anthony <nvhoanganh1909@gmail.com>
2019-08-22 23:41:14 +1000

    added root store

--- apps/avatar/src/app/app.module.ts
+++ apps/avatar/src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { CoreModule } from '@avatar/core'
+import { CoreModule } from '@avatar/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromApp from './+state/app.reducer';
+import { AppEffects } from './+state/app.effects';
+import { NxModule } from '@nrwl/angular';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreModule.forFeature(fromApp.APP_FEATURE_KEY, fromApp.reducer)
   ],
   providers: [],
   bootstrap: [AppComponent]
